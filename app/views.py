13b73d589e66a38c49c230f76f39d82677174038
Chanchal Khiani <ckhiani@coolcorp.com>
2026-03-16 22:42:24 -0500

    Styled image grid for uploaded files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
@@ -11,6 +11,28 @@ from app.forms import LoginForm, UploadForm
 ###
 # Routing for your application.
 ###
+def get_uploaded_images():
+    folder = os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'])
+    images = []
+
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            images.append(file)
+
+    return images
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    return send_from_directory(
+        os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']),
+        filename
+    )
+
+@app.route('/files')
+@login_required
+def files():
+    images = get_uploaded_images()
+    return render_template('files.html', images=images)
 
 @app.route('/')
 def home():
@@ -62,10 +84,10 @@ def login():
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
+
 @login_manager.user_loader
 def load_user(id):
-    return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
-
+    return db.session.execute(db.select(UserProfile).filter_by(id=int(id))).scalar()
 ###
 # The functions below should be applicable to all Flask apps.
 ###
