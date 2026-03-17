1fa2b4e27cc19359736809368ca63f21957d3ca4
Chanchal Khiani <ckhiani@coolcorp.com>
2026-03-16 22:52:59 -0500

    Completed Lab 4 - authentication, upload, image display, logout

--- app/views.py
+++ app/views.py
@@ -82,6 +82,13 @@ def login():
 
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('home'))
+
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 
